3c03031a6874d136488f4f65aa2d575757c08698
Johan Backman <johan@cultivateai.com>
2019-06-06 21:41:57 -0700

    Store UUID as string in DB

--- services/auth/db/models.go
+++ services/auth/db/models.go
@@ -8,22 +8,22 @@ import (
 )
 
 type Base struct {
-	ID        uuid.UUID  `gorm:"type:uuid;primary_key"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt *time.Time `sql:"index"`
+	ID        uuid.UUID  `gorm:"varchar(36);primary_key"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"update_at"`
+	DeletedAt *time.Time `sql:"index" json:"deleted_at"`
 }
 
 // BeforeCreate will set a UUID rather than numeric ID.
 func (base *Base) BeforeCreate(scope *gorm.Scope) error {
 	uuid := uuid.NewV4()
-	return scope.SetColumn("ID", uuid)
+	return scope.SetColumn("ID", uuid.String())
 }
 
 type Account struct {
 	Base
-	Username string `gorm:"varchar(100);not null;unique_index"`
-	Email    string `gorm:"varchar(100);not null;unique_index"`
-	Password string `gorm:"column:password_hash;varchar(256);not null"`
-	EmailVerifiedAt *time.Time
+	Username string `gorm:"varchar(100);not null;unique_index" json:"username"`
+	Email    string `gorm:"varchar(100);not null;unique_index" json:"email"`
+	Password string `gorm:"column:password_hash;varchar(256);not null" json:"password_hash"`
+	EmailVerifiedAt *time.Time `json:"email_verified_at"`
 }
