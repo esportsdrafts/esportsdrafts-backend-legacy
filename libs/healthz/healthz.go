f1e9e5d1c69df18675ff2b3e4c23c6bf482b9960
Johan Backman <johan@cultivateai.com>
2019-07-26 10:07:54 -0700

    Add logging middleware using logrus

--- libs/healthz/healthz.go
+++ libs/healthz/healthz.go
@@ -1,11 +1,13 @@
 package healthz
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
 func healthz(ctx echo.Context) error {
-	return nil
+	return ctx.NoContent(http.StatusOK)
 }
 
 func RegisterHealthChecks(router echo.Echo) {
---
+++ libs/log/log.go
@@ -0,0 +1,74 @@
+package efanlog
+
+import (
+	"encoding/json"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+)
+
+func GetLogger() *logrus.Logger {
+	logger := logrus.New()
+	env := os.Getenv("ENV")
+
+	if env == "production" || env == "prod" {
+		logger.SetFormatter(&logrus.JSONFormatter{})
+	}
+	logger.SetOutput(os.Stdout)
+
+	return logger
+}
+
+func EchoLoggingMiddleware() echo.MiddlewareFunc {
+	return loggingMiddleware()
+}
+
+func loggingMiddleware() echo.MiddlewareFunc {
+	logger := GetLogger()
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) (err error) {
+			req := c.Request()
+			res := c.Response()
+			start := time.Now()
+			if err = next(c); err != nil {
+				c.Error(err)
+			}
+			stop := time.Now()
+
+			requestID := req.Header.Get(echo.HeaderXRequestID)
+			if requestID == "" {
+				requestID = res.Header().Get(echo.HeaderXRequestID)
+			}
+			latency := stop.Sub(start)
+
+			requestPath := req.URL.Path
+			if requestPath == "" {
+				requestPath = "/"
+			}
+
+			requestError := ""
+			if err != nil {
+				b, _ := json.Marshal(err.Error())
+				requestError = string(b[1 : len(b)-1])
+			}
+
+			// TODO: Add userID from auth token
+			logger.WithFields(logrus.Fields{
+				"id":            requestID,
+				"remote_ip":     c.RealIP(),
+				"user_agent":    req.UserAgent(),
+				"status":        res.Status,
+				"latency":       strconv.FormatInt(int64(latency), 10),
+				"latency_human": stop.Sub(start).String(),
+				"method":        req.Method,
+				"path":          requestPath,
+				"error":         requestError,
+			}).Info(req.Method + " " + req.RequestURI)
+
+			return nil
+		}
+	}
+}
