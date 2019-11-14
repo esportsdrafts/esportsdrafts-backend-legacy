710a2f747d682fa6595a35caa7b07a5d592b8093
Johan Backman <johan@cultivateai.com>
2019-11-14 08:18:29 -0800

    Add authlib tests

--- libs/authlib/authlib_test.go
+++ libs/authlib/authlib_test.go
@@ -1,10 +1,13 @@
 package authlib
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo/v4"
 )
 
 func TestReconstructToken(t *testing.T) {
@@ -33,6 +36,46 @@ func TestCreateMiddleware(t *testing.T) {
 		},
 	)
 	// TODO
+}
+
+func TestJWTRace(t *testing.T) {
+	e := echo.New()
+	handler := func(c echo.Context) error {
+		return c.String(http.StatusOK, "test")
+	}
+	initialToken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"
+	raceToken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlJhY2UgQ29uZGl0aW9uIiwiYWRtaW4iOmZhbHNlfQ.Xzkx9mcgGqYMTkuxSCbJ67lsDyk5J2aB7hu65cEE-Ss"
+	validKey := []byte("secret")
+
+	h := JWTMiddleware(JWTConfig{
+		AllowedRole: "pelle",
+		SigningKey:  validKey,
+	})(handler)
+
+	makeReq := func(token string) echo.Context {
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		res := httptest.NewRecorder()
+		req.Header.Set(echo.HeaderAuthorization, "Bearer: "+token)
+		c := e.NewContext(req, res)
+
+		reqResult := h(c)
+		if reqResult != nil {
+			t.Errorf("Failed to perfom request: %e", reqResult)
+		}
+		return c
+	}
+
+	c := makeReq(initialToken)
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(*JWTClaims)
+	assert.Equal(t, claims.Name, "John Doe")
+
+	makeReq(raceToken)
+	user = c.Get("user").(*jwt.Token)
+	claims = user.Claims.(*jwtCustomClaims)
+	// Initial context should still be "John Doe", not "Race Condition"
+	assert.Equal(t, claims.Name, "John Doe")
+	assert.Equal(t, claims.Admin, true)
 }
 
 func TestShouldSetCookie(t *testing.T) {
